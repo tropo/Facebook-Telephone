fb9a681028911e3c1d54e314af24b0b911d85afa
Topher <chris@matthieu.us>
2010-10-01 17:35:32 -0700

    added answer from browser

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -25,38 +25,67 @@ class FacebookController < ApplicationController
   end
   
   def telephone
-    # format DID or SIP address properly
-    phone = params[:id] 
-    if phone
-      phone = phone.gsub("-", "").gsub("(", "").gsub(")", "").gsub("+", "")
-      if phone.index("@") and !phone.index("sip:")
-        phone = "sip:" + phone
-      end
-  
-      if isNumeric(phone) and phone.length == 10
-        phone = "1" + phone
-      end
+    # # format DID or SIP address properly -> /telephone/4805551212 or /telephone/sip:abc@sip.com
+    # phone = params[:id] 
+    # if phone
+    #   phone = phone.gsub("-", "").gsub("(", "").gsub(")", "").gsub("+", "")
+    #   if phone.index("@") and !phone.index("sip:")
+    #     phone = "sip:" + phone
+    #   end
+    #   
+    #   if isNumeric(phone) and phone.length == 10
+    #     phone = "1" + phone
+    #   end
+    # 
+    #   #Setup Dial (DID or SIP) | http://phono.com/16025551212
+    #   if isNumeric(phone) 
+    #     @did = phone
+    #     # @phone = "sip:9991443313@sip.tropo.com" #;postd=p16025551212;pause=1000ms
+    #     # @phone = "sip:9991443313@stagingsbc-external.orl.voxeo.net"
+    #     # @phone = "sip:9991443313@sbcexternal.orl.voxeo.net"
+    #     @phone = "app:9991443313"
+    #   
+    #   elsif phone.index('@') or phone.index('app:')
+    #     @did = ""
+    #     @phone = phone
+    # 
+    #   else
+    #     # @did = "app:9991443124"
+    #     # @phone = "app:9991443124"
+    #     # @short = @phone
+    #   
+    #   end
+    # end
     
-      #Setup Dial (DID or SIP) | http://phono.com/16025551212
-      if isNumeric(phone) 
-        @did = phone
-        # @phone = "sip:9991443313@sip.tropo.com" #;postd=p16025551212;pause=1000ms
-        # @phone = "sip:9991443313@stagingsbc-external.orl.voxeo.net"
-        # @phone = "sip:9991443313@sbcexternal.orl.voxeo.net"
-        @phone = "app:9991443313"
-      
-      elsif phone.index('@') or phone.index('app:')
+    # Look up user by id and call their SIP address and posted number
+    userid = params[:id]
+    if userid
+      @user = User.find_by_facebookid(userid)
+      if @user
+        @transfermode = "one" # "all" = simultaneous rings or "one" = one phone at a time
         @did = ""
-        @phone = phone
-    
-      else
-        # @did = "app:9991443124"
-        # @phone = "app:9991443124"
-        # @short = @phone
-      
+        if @user.sip
+          sipraw = @user.sip
+          if sipraw.index("@") and !sipraw.index("sip:")
+            sipraw = "sip:" + sipraw
+          end
+          @did << sipraw + ","
+        end
+        if @user.phonenumber
+          phoneraw = @user.phonenumber.gsub("-", "").gsub("(", "").gsub(")", "").gsub("+", "")
+          if phoneraw.index("@") and !phoneraw.index("sip:")
+            phoneraw = "sip:" + phoneraw
+          end
+          if isNumeric(phoneraw) and phoneraw.length == 10
+            phoneraw = "1" + phoneraw
+          end
+          @did << phoneraw + ","
+        end
+        @did = @did.chop #remove last comma from @did string
+        @phone = "app:9991443419"
       end
     end
-    
+    puts @did
     render 'phono', :layout => false
     
   end
