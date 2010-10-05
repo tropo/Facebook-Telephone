c57bb0b1975b66af17878cc39b0359d8ab3a8e5a
Topher <chris@matthieu.us>
2010-10-05 07:55:22 -0700

    implemented oauth2 gem2

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -13,7 +13,33 @@ class OauthController < ApplicationController
     user_json = access_token.get('/me')
     # in reality you would at this point store the access_token.token value as well as 
     # any user info you wanted
-    render :json => user_json
+    # render :json => user_json
+    
+    if !user_json.nil? 
+      
+      @user = User.find_by_facebookid(user_json["id"])
+      if @user.nil?
+        @user = User.new
+      end
+
+      @user.facebookid = user_json["id"]
+      @user.name = user_json["name"]
+      @user.firstname = user_json["first_name"]
+      @user.lastname = user_json["last_name"]
+      @user.link = user_json["link"]
+      @user.gender = user_json["gender"]
+      @user.email = user_json["email"]
+      @user.timezone = user_json["timezone"]
+      @user.local = user_json["locale"]
+      @user.verified = user_json["verified"]
+      @user.token = access_token.token
+      @user.save
+      
+      session["id"] = user_json["id"]
+      session["usertoken"] = access_token.token
+
+    end
+    
   end
 
   protected
