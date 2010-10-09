d21abebccb9b020dc3524e509d3067be17a9e845
Topher <chris@matthieu.us>
2010-10-09 07:48:13 -0700

    oauth2 changes3

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ Facebooktelephone::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "facebook#index"
+  root :to => "oauth#start"
 
   # See how all your routes lay out with "rake routes"
 
