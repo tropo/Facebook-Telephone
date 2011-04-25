5116c90e508af32ba9e816404cf732156d11a06c
Chris Matthieu <chris@matthieu.us>
2011-04-25 15:58:27 -0700

    updated openssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery
   
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE  
+  
   def isNumeric(s)
       Float(s) != nil rescue false
   end
